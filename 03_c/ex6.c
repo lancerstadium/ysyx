e04d076cc26d36cffab361a0aff115b9c0cf8093
lancerstadium <lancerstadium@163.com>
2024-05-16 22:53:11 +0800

    1: 2024/5/16

---
+++ 03_c/ex6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    float power = 2.345f;
+    double super_power = 56789.4532;
+    char initial = 'A';
+    char first_name[] = "Zed";
+    char last_name[] = "Shaw";
+
+    int height = 624;
+    int weight = 137;
+
+    printf("You are %d miles away.\n", distance);
+    printf("You have %f levels of power.\n", power);
+    printf("You have %f awesome super powers.\n", super_power);
+    printf("I have an initial %c.\n", initial);
+    printf("I have a first name %s.\n", first_name);
+    printf("I have a last name %s.\n", last_name);
+    printf("My whole name is %s %c. %s.\n",
+            first_name, initial, last_name);
+
+    printf("Height: %x\n", height);
+    printf("Weight: %o\n", weight);
+
+    return 0;
+}
